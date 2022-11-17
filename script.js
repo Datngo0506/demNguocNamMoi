5f59966c39cf98d3bf03550931618a6bb8c16b9c
Datngo0506 <91180042+Datngo0506@users.noreply.github.com>
2022-11-17 18:06:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let dateNewYear = new Date('Jan 22, 2023 00:00:00').getTime();
+    function refreshCounter() {
+        let dateCurrent = new Date().getTime(),
+            timeLeft = dateNewYear - dateCurrent;
+        let second = 1000,
+            minute = second * 60,
+            hour = minute * 60,
+            day = hour * 24;
+        let tLDay = zeroPad(Math.floor(timeLeft / day), 2),
+            tLHour = zeroPad(Math.floor((timeLeft % day) / hour), 2),
+            tLMinute = zeroPad(Math.floor((timeLeft % hour) / minute), 2),
+            tLSecond = zeroPad(Math.floor((timeLeft % minute) / second), 2);
+        function zeroPad(num, count) {
+            return num.toString().padStart(count, "0");
+        }
+        document.getElementById("day").innerText = tLDay;
+        document.getElementById("hour").innerText = tLHour;
+        document.getElementById("minute").innerText = tLMinute;
+        document.getElementById("second").innerText = tLSecond;
+    }
+    setInterval(() => {
+        refreshCounter();
+    }, 1000);
